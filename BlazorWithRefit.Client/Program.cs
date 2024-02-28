1f2d144b901c34933270200b294fb53cff56a6c0
Saji Weerasingham <saji.weerasingham@outlook.com>
2024-02-29 05:29:18 +1000

    added client base url config

--- BlazorWithRefit.Client/Program.cs
+++ BlazorWithRefit.Client/Program.cs
@@ -4,7 +4,9 @@ using Refit;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.Services.AddRefitClient<IWeatherApi>();
+builder.Services
+    .AddRefitClient<IWeatherApi>()
+    .ConfigureHttpClient(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
 var app = builder.Build();
 
